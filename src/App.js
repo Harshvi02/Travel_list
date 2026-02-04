fefa08d5a2b87666b878b9c891b9bea3cf8205be
Harshvi02 <230493131002.harshvi@gmail.com>
2026-02-04 11:02:47 +0530

    Initial pizza travellist UI design

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+const initialItems = [];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
     </div>
+  );
+}
+
+function Logo() {
+  return <h1>🌴 FAR AWAY 🧳</h1>;
+}
+
+function Form() {
+  return (
+    <form className="add-form">
+      <h3>What do you need for your 😍 trip?</h3>
+
+      <select>
+        <option value={1}>1</option>
+        <option value={2}>2</option>
+        <option value={3}>3</option>
+      </select>
+
+      <input type="text" placeholder="Item..." />
+      <button>Add</button>
+    </form>
+  );
+}
+
+function PackingList() {
+  return (
+    <div className="list">
+      <p>List will come here</p>
+    </div>
+  );
+}
+
+function Stats() {
+  return (
+    <footer className="stats">
+      <em>Start adding some items to your packing list 🚀</em>
+    </footer>
   );
 }
 
